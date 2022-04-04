ad53b6e75163177834b49f0b6d95d977d2bfae58
Genesis-UA <64205209+Genesis-UA@users.noreply.github.com>
2022-04-04 17:02:52 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo apt update -qq -y
+sudo apt install git python3 python3-pip -qq -y
+# for some virtual cloud systems based on debian (like GC)
+#sudo apt install gcc libc-dev libffi-dev libssl-dev python3-dev rustc -qq -y 
+sudo pip install --upgrade pip
