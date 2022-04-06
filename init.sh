e52441cdbabcb7f2482e309c7cce5f6f4e9e658b
Genesis-UA <64205209+Genesis-UA@users.noreply.github.com>
2022-04-06 20:37:50 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 # curl -s https://raw.githubusercontent.com/Genesis-UA/runner/main/init.sh | bash
 
-sudo apt update -qq -y
-sudo apt install git python3 python3-pip -qq -y
-# for some virtual cloud systems based on debian (like GC)
-#sudo apt install gcc libc-dev libffi-dev libssl-dev python3-dev rustc -qq -y 
-sudo pip install --upgrade pip
+cd ~  
+sudo rm -r auto_mhddos_alexnest
+sudo apt install git -y  
+git clone https://github.com/alexnest-ua/auto_mhddos_alexnest
+cd ~/auto_mhddos_alexnest
+bash setup.sh
