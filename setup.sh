c404b4e398406824920e6be821aa5235f7b40147
Genesis-UA <64205209+Genesis-UA@users.noreply.github.com>
2022-04-10 18:33:41 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#Just in case kill previous copy of mhddos_proxy
+sudo pkill -f runner.sh
+sudo pkill -f runner.py
+sudo pkill -f ./start.py
+
+
+sudo apt update -y
+sudo apt upgrade -y
+# Install git, python3
+sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
+sudo apt install git -y
+sudo apt upgrade git -y
+sudo apt install wget -y
+sudo apt upgrade wget -y
+sudo apt install python3 -y
+sudo apt upgrade python3 -y
+sudo apt install python3-pip -y
+sudo apt upgrade python3-pip -y
+sudo apt install screen -y
+sudo apt upgrade screen -y
+sudo apt install curl -y
+sudo apt upgrade curl -y
+sudo -H pip3 install --upgrade pip
