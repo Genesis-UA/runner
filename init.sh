0ee3926ed9f072b4095ab611659c143da1d57f64
Genesis-UA <64205209+Genesis-UA@users.noreply.github.com>
2022-04-10 11:21:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 # curl -s https://raw.githubusercontent.com/Genesis-UA/runner/main/init.sh | bash
 
-cd ~  
+sudo apt update -y
+cd ~
 sudo rm -r auto_mhddos_alexnest
-sudo apt install git -y  
+sudo apt install git -y
 git clone https://github.com/alexnest-ua/auto_mhddos_alexnest
 cd ~/auto_mhddos_alexnest
 bash setup.sh
