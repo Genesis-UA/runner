03241131e17dc6ff63ca07182520f9038ba45143
Genesis-UA <64205209+Genesis-UA@users.noreply.github.com>
2022-04-13 19:47:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,24 +3,16 @@
 #Just in case kill previous copy of mhddos_proxy
 sudo pkill -f runner.sh
 sudo pkill -f runner.py
-sudo pkill -f ./start.py
 
 cd ~
-sudo rm -r runner
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt install wget -y
-sudo apt upgrade wget -y
-sudo apt install python3 -y
-sudo apt upgrade python3 -y
-sudo apt install python3-pip -y
-sudo apt upgrade python3-pip -y
-sudo apt install screen -y
-sudo apt upgrade screen -y
-#sudo apt install curl -y
-#sudo apt upgrade curl -y
+sudo apt install --upgrade git -y
+sudo apt install --upgrade python3 -y
+sudo apt install --upgrade python3-pip -y
 sudo -H pip3 install --upgrade pip
-sudo apt install git -y
-git clone https://github.com/Genesis-UA/runner
-cd ~/runner
-bash runner.sh
+sudo apt install --upgrade screen -y
+sudo rm -r mhddos_proxy
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+ulimit -n 1048576
+sudo python3 -m pip install -r requirements.txt
